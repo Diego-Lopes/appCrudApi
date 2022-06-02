a764b8075e4c42072289bc1f19204ac085997595
Diego-Lopes <diegolopes2009@hotmail.com>
2022-06-01 21:43:09 -0400

    teste nova interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public appPages = [
     { title: 'Home', url: '/home', icon: 'home' },
-    { title: 'Cliente', url: '/cliente', icon: 'paper-plane' },
-    { title: 'Produtos', url: '/produto', icon: 'heart' },
+    { title: 'Cadastrar fone', url: '/cliente', icon: 'paper-plane' },
+    // { title: 'Produtos', url: '/produto', icon: 'heart' },
   ];
   // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
